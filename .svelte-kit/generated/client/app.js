b2bebb1303614bcd2de7a721443071baaaa2657a
bilza2023 <bilza2023@gmail.com>
2025-05-04 21:08:34 +0500

    admin moved to admin

--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -11,21 +11,23 @@ export const nodes = [
 	() => import('./nodes/7'),
 	() => import('./nodes/8'),
 	() => import('./nodes/9'),
-	() => import('./nodes/10')
+	() => import('./nodes/10'),
+	() => import('./nodes/11')
 ];
 
 export const server_loads = [];
 
 export const dictionary = {
 		"/": [2],
-		"/admin": [3],
-		"/blog": [4],
-		"/canvas_slides_index": [6],
-		"/canvas_slides": [5],
-		"/editor": [7],
-		"/player": [8],
-		"/static_player": [9],
-		"/syllabus": [10]
+		"/about": [3],
+		"/admin": [4],
+		"/blog": [5],
+		"/canvas_slides_index": [7],
+		"/canvas_slides": [6],
+		"/editor": [8],
+		"/player": [9],
+		"/static_player": [10],
+		"/syllabus": [11]
 	};
 
 export const hooks = {
--- .svelte-kit/generated/client/nodes/11.js
+++ .svelte-kit/generated/client/nodes/11.js
@@ -1,1 +1,1 @@
-export { default as component } from "../../../../src/routes/static_slides/+page.svelte";
+export { default as component } from "../../../../src/routes/syllabus/+page.svelte";
